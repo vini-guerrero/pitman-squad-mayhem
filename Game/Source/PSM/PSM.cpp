f29e6a094eb7bf41b971061febaf691f1d5c64b0
Vinicius <vini.ts.guerrero@gmail.com>
2023-01-13 19:37:39 -0300

    C++ Project

---
+++ Game/Source/PSM/PSM.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PSM.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PSM, "PSM" );
